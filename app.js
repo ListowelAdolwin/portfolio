3d979511498d16e3be7c688b58ad2fbbf7c70ff2
ListowelAdolwin <listoweladolwin@gmail.com>
2022-11-04 21:04:46 +0000

    JavaScript

---
+++ app.js
@@ -0,0 +1,15 @@
+var tabLinks = document.getElementsByClassName('tab-links');
+var tabContents = document.getElementsByClassName('tab-content');
+
+function opentab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove('active-tab-link');
+    }
+
+    for(tabContent of tabContents){
+        tabContent.classList.remove('active-tab-content');
+    }
+
+    event.currentTarget.classList.add("active-tab-link");
+    document.getElementById(tabname).classList.add('active-tab-content');
+}
