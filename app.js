28fdd776ef3a51036303727d0473153afb7fd5d4
ListowelAdolwin <listoweladolwin@gmail.com>
2022-12-13 19:02:20 +0000

    Activated the see more button

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-var tabLinks = document.getElementsByClassName('tab-links');
-var tabContents = document.getElementsByClassName('tab-content');
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-content");
 
-function opentab(tabname){
-    for(tabLink of tabLinks){
-        tabLink.classList.remove('active-tab-link');
-    }
+function opentab(tabname) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-tab-link");
+  }
 
-    for(tabContent of tabContents){
-        tabContent.classList.remove('active-tab-content');
-    }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab-content");
+  }
 
-    event.currentTarget.classList.add("active-tab-link");
-    document.getElementById(tabname).classList.add('active-tab-content');
+  event.currentTarget.classList.add("active-tab-link");
+  document.getElementById(tabname).classList.add("active-tab-content");
 }
 
+/** Show hidden projects */
+function showProjects() {
+  var hidden_projects = document.getElementsByClassName("hidden-project");
+  var show_more = document.getElementById('show-more');
+  for (project of hidden_projects) {
+    if (project.style.display == 'none'){
+    project.style.display = "block";
+    show_more.innerHTML = "See less"
+    } else {
+        project.style.display = "none";
+        show_more.innerHTML = 'See more'
+    }
+}
+}
+
+
+/*
+
+function showPrjects() {
+    var hidden_projects = document.getElementsByClassName("hidden-project");
+
+    for (project of hidden_projects){
+        var display = getComputedStyle('project').display;
+        if (display == "none") {
+            searchbar.style.display = "block";
+        } else {
+            searchbar.style.display = "none";
+        }
+    }
+
+    }
+ */
